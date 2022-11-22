3d44680a0d31161974611210bda4b4b1202eb55f
AdamWalker-Tonkean <adam.walker@tonkean.com>
2022-11-22 13:46:39 +0200

    remove node modules and add markers to maps

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ export default function App(){
 
   const [coordinates, setCoordinates] = useState({ lat: -34.397, lng: 150.644 })
   const [zoom, setZoom] = useState(8)
+  const [markerDetails, setMarkerDetails] = useState({ title: '', shop: '' })
+  const [markerCount, setMarkerCount] = useState(0);
   const input = useRef(null)
 
   function reposition(city) {
@@ -28,17 +30,27 @@ export default function App(){
     setZoom(Number(input.value))
   };
 
+  function showLocation() {
+    window.navigator.geolocation.getCurrentPosition((location) => {
+      const { coords: {latitude: lat, longitude: lng } } = location
+      setCoordinates({lat,lng})
+    })
+  }
+
+  const updateDetails = event => {
+    setMarkerDetails({...markerDetails, [event.target.name]: event.target.value})
+  }
+
+ 
+
 
     return (
       <div className="app">
         <TopBar>Google Maps Example in React</TopBar>
         <div className="hbox mb20">
           {/* How should we format components with lots of props?  */}
-          <button
-            data-city="london"
-            onClick={() => reposition("tel aviv")}
-          > Tel Aviv </button>
-          <button data-city="paris" onClick={() => reposition("paris")}> Paris </button>
+          <button onClick={() => reposition("tel aviv")}> Tel Aviv </button>
+          <button onClick={() => reposition("paris")}> Paris </button>
           <button onClick={() => reposition("london")}>London</button>
           <input
             ref={input}
@@ -48,8 +60,22 @@ export default function App(){
             placeholder="8"
             onChange={updateZoom}
           />
+        <button onClick={showLocation}>Where Am I?</button>
         </div>
-        <GoogleMap zoom={zoom} lat={coordinates.lat} lng={ coordinates.lng} />
+        
+        <div className="hbox mb20">
+          <input name='title' onChange={updateDetails} type='text'/>
+          <select name='shop' onChange={updateDetails}>
+            <option value=''>None</option>
+            <option value='starbucks'>StarBucks</option>
+            <option value='costa'>Costa Coffee</option>
+            <option value='waterstones'>Waterstone</option>
+            <option value='next'>Next</option>
+            <option value='marks'>Marks & Spencers</option>
+          </select>
+          <button onClick={() => setMarkerCount(markerCount + 1)}>Add Marker</button>
+        </div>
+        <GoogleMap {...{zoom, markerCount, markerDetails}} lat={coordinates.lat} lng={coordinates.lng} />
       </div>
     )
 }
--- src/components/Google_map.js
+++ src/components/Google_map.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef } from "react";
 
-export default function GoogleMap({ lat, lng, zoom }) {
+export default function GoogleMap({ lat, lng, zoom, markerCount, markerDetails }) {
 
   // Global Refs to the Map Container and the Google Map Object - They are created after the component is rendered
   const mapContainer = useRef(null)
@@ -22,6 +22,26 @@ export default function GoogleMap({ lat, lng, zoom }) {
   useEffect(() => {
     map.current.setZoom(zoom);
   }, [zoom])
+
+  useEffect(() => {
+    const contentString = `<div><h1>${markerDetails.title}</h1><h2>${markerDetails.shop}</h2></div>`
+    const infoWindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+
+    let marker = new google.maps.Marker();
+    marker.setPosition(map.current.getCenter().toJSON())
+    marker.setMap(map.current);
+    marker.addListener('mouseover', event => {infoWindow.open({
+      anchor:marker,
+      map: map.current,
+    })})
+    marker.addListener('mouseout', event => {infoWindow.close()})
+  }, [markerCount])
+
+  // How do we stop the div from rerendering without "shouldComponentUpdate"?
   
-  return <div ref={ mapContainer } className="map-box" />
+  return <>
+    <div ref={mapContainer} className="map-box"></div>
+  </>
 }
